c12f388a93fdb73004986f61f7650ec9295a1a1c
superroket169 <isagullu2010@gmail.com>
2026-06-21 10:53:08 +0300

    feat: mod.rs update

--- src/nn/mod.rs
+++ src/nn/mod.rs
@@ -1,6 +1,16 @@
+pub mod add;
+pub mod attention;
 pub mod linear;
 pub mod pipeline;
+pub mod rmsnorm;
+pub mod rope;
+pub mod silu;
 pub mod traits;
 
 pub use linear::Linear;
 pub use traits::Layer;
+
+pub use add::Add;
+pub use rmsnorm::RMSNorm;
+pub use rope::RoPE;
+pub use silu::SiLU;
