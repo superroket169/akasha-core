0c5eff1803a7e32fd4897743eca55f1115889d5a
superroket169 <isagullu2010@gmail.com>
2026-06-21 15:32:32 +0300

    feat: add func new implemented for backward and Layer implemented for add

--- src/nn/add.rs
+++ src/nn/add.rs
@@ -1,3 +1,4 @@
+use super::traits::Layer;
 use filuplex::context::Context;
 use filuplex::graph::{ComputeGraphBuilder, ExecutableGraph};
 use filuplex::ops::{BuiltInShader, GpuBuffer};
@@ -5,32 +6,65 @@ use std::sync::Arc;
 
 pub struct Add {
     pub in_out_buffer: GpuBuffer,
-    pub graph: ExecutableGraph,
+    pub grad_a: GpuBuffer,
+    pub grad_b: GpuBuffer,
+    pub forward_graph: ExecutableGraph,
+    pub backward_graph: ExecutableGraph,
 }
 
 impl Add {
-    pub fn new(ctx: Arc<Context>, length: u32, buf_a: &GpuBuffer, buf_b: &GpuBuffer) -> Self {
+    pub fn new(
+        ctx: Arc<Context>,
+        length: u32,
+        buf_a: &GpuBuffer,
+        buf_b: &GpuBuffer,
+        grad_output: &GpuBuffer,
+    ) -> Self {
         let meta_data = vec![length as f32];
         let meta = GpuBuffer::from_cpu(&meta_data, &ctx);
 
-        let shader = BuiltInShader::load_from_file(&ctx, "src/shaders/add_inplace.spv").load(&ctx);
-        let mut builder = ComputeGraphBuilder::new(ctx.clone());
-        builder.add_operation(
-            shader,
+        let shader_fw =
+            BuiltInShader::load_from_file(&ctx, "src/shaders/add_inplace.spv").load(&ctx);
+        let mut fw_builder = ComputeGraphBuilder::new(ctx.clone());
+        fw_builder.add_operation(
+            shader_fw,
             vec![(0, buf_a), (1, buf_b), (2, &meta)],
+            [(length + 255) / 256, 1, 1],
+        );
+
+        let grad_a = GpuBuffer::from_cpu(&vec![0.0f32; length as usize], &ctx);
+        let grad_b = GpuBuffer::from_cpu(&vec![0.0f32; length as usize], &ctx);
+
+        let shader_bwd = BuiltInShader::load_from_file(&ctx, "src/shaders/add.spv").load(&ctx);
+        let mut bw_builder = ComputeGraphBuilder::new(ctx.clone());
+
+        bw_builder.add_operation(
+            shader_bwd.clone(),
+            vec![(0, &grad_a), (1, grad_output), (2, &meta)],
+            [(length + 255) / 256, 1, 1],
+        );
+        bw_builder.add_operation(
+            shader_bwd.clone(),
+            vec![(0, &grad_b), (1, grad_output), (2, &meta)],
             [(length + 255) / 256, 1, 1],
         );
 
         Self {
             in_out_buffer: buf_a.clone(),
-            graph: builder.build(),
+            grad_a,
+            grad_b,
+            forward_graph: fw_builder.build(),
+            backward_graph: bw_builder.build(),
         }
     }
 }
 
-impl Add {
-    pub fn forward(&self) -> GpuBuffer {
-        self.graph.execute();
-        self.in_out_buffer.clone()
+impl Layer for Add {
+    fn forward(&self) {
+        self.forward_graph.execute();
+    }
+
+    fn backward(&self) {
+        self.backward_graph.execute();
     }
 }
