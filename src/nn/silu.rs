8d060f6f896f9844485352149e6b1c97e4e23955
superroket169 <isagullu2010@gmail.com>
2026-06-21 14:55:33 +0300

    feat: new functions updated with backwards

--- src/nn/silu.rs
+++ src/nn/silu.rs
@@ -6,11 +6,18 @@ use std::sync::Arc;
 
 pub struct SiLU {
     pub in_out_buffer: GpuBuffer,
+    pub grad_input: GpuBuffer,
     pub graph: ExecutableGraph,
+    pub backward_graph: ExecutableGraph,
 }
 
 impl SiLU {
-    pub fn new(ctx: Arc<Context>, length: u32, input_buffer: &GpuBuffer) -> Self {
+    pub fn new(
+        ctx: Arc<Context>,
+        length: u32,
+        input_buffer: &GpuBuffer,
+        grad_output: &GpuBuffer,
+    ) -> Self {
         let meta_data = vec![length as f32];
         let meta = GpuBuffer::from_cpu(&meta_data, &ctx);
 
@@ -22,9 +29,22 @@ impl SiLU {
             [(length + 255) / 256, 1, 1],
         );
 
+        // --- BACKWARD ---
+        let grad_input = GpuBuffer::from_cpu(&vec![0.0f32; length as usize], &ctx);
+        let mut bw_builder = ComputeGraphBuilder::new(ctx.clone());
+        let shader_bwd = BuiltInShader::load_from_file(&ctx, "src/shaders/silu_bwd.spv").load(&ctx);
+
+        bw_builder.add_operation(
+            shader_bwd,
+            vec![(0, input_buffer), (1, grad_output), (2, &grad_input)],
+            [(length + 255) / 256, 1, 1],
+        );
+
         Self {
             in_out_buffer: input_buffer.clone(),
+            grad_input,
             graph: builder.build(),
+            backward_graph: bw_builder.build(),
         }
     }
 }
@@ -32,11 +52,9 @@ impl SiLU {
 impl Layer for SiLU {
     fn forward(&self) {
         self.graph.execute();
-        // self.in_out_buffer.clone()
     }
-
     fn backward(&self) {
-        // TODO
+        self.backward_graph.execute();
     }
 }
 
