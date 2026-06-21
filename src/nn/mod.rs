eb5b2a5f81abd994824e83769208d97eb842016f
superroket169 <isagullu2010@gmail.com>
2026-06-21 13:28:30 +0300

    feat: mod.rs update

--- src/nn/mod.rs
+++ src/nn/mod.rs
@@ -1,14 +1,18 @@
 pub mod add;
+pub mod akasha_model;
 pub mod attention;
+pub mod embedding;
 pub mod linear;
 pub mod pipeline;
 pub mod rmsnorm;
 pub mod rope;
 pub mod silu;
 pub mod traits;
+pub mod weights;
 
 pub use linear::Linear;
 pub use traits::Layer;
+pub use traits::Serializable;
 
 pub use add::Add;
 pub use rmsnorm::RMSNorm;
