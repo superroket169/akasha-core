c7e6eff39e45deb7b8aef7445d76b55e590424b8
superroket169 <isagullu2010@gmail.com>
2026-06-21 10:05:17 +0300

    feat: linear first version fully fins

--- src/nn/linear.rs
+++ src/nn/linear.rs
@@ -1,29 +1,55 @@
 use super::traits::Layer;
 use filuplex::context::Context;
-use filuplex::ops::{BuiltInShader, BuiltInShaderType, GpuBuffer};
+use filuplex::graph::{ComputeGraphBuilder, ExecutableGraph};
+use filuplex::ops::{BuiltInShader, GpuBuffer};
 use std::sync::Arc;
 
 pub struct Linear {
-    pub weight: GpuBuffer,
-    pub shader: BuiltInShader,
-    pub ctx: Arc<Context>,
+    pub out_buffer: GpuBuffer,
+    pub graph: ExecutableGraph,
 }
 
 impl Linear {
-    pub fn new(ctx: Arc<Context>, weight_data: &[f32]) -> Self {
-        let weight_buf = GpuBuffer::from_cpu(weight_data, &ctx);
-        let shader = BuiltInShader::new(BuiltInShaderType::MatrisMul);
+    pub fn new(
+        ctx: Arc<Context>,
+        in_features: u32,
+        out_features: u32,
+        weight_data: &[f32],
+        input_buffer: &GpuBuffer,
+    ) -> Self {
+        let weight = GpuBuffer::from_cpu(weight_data, &ctx);
 
-        Self {
-            weight: weight_buf,
+        let seq_len = 1;
+        let m = seq_len as u32;
+
+        let meta_data = vec![m as f32, in_features as f32, out_features as f32];
+        let meta = GpuBuffer::from_cpu(&meta_data, &ctx);
+
+        let dummy_out = vec![0.0f32; (m * out_features) as usize];
+        let out_buffer = GpuBuffer::from_cpu(&dummy_out, &ctx);
+
+        let shader = BuiltInShader::load_from_file(&ctx, "src/shaders/matmul.spv").load(&ctx);
+
+        let mut builder = ComputeGraphBuilder::new(ctx.clone());
+        builder.add_operation(
             shader,
-            ctx,
-        }
+            vec![
+                (0, input_buffer),
+                (1, &weight),
+                (2, &out_buffer),
+                (3, &meta),
+            ],
+            [(out_features + 15) / 16, (m + 15) / 16, 1],
+        );
+        let graph = builder.build();
+
+        Self { out_buffer, graph }
     }
 }
 
 impl Layer for Linear {
-    fn forward(&self, input: &GpuBuffer) -> GpuBuffer {
-        todo!("theres the actualy part")
+    fn forward(&self, _input: &GpuBuffer) -> GpuBuffer {
+        self.graph.execute();
+        self.out_buffer.clone()
     }
 }
