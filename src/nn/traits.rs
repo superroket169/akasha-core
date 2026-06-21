79bdb73ca363f9a8cfe1aca9c728cad1a48c7872
superroket169 <isagullu2010@gmail.com>
2026-06-21 13:50:44 +0300

    test commit

--- src/nn/traits.rs
+++ src/nn/traits.rs
@@ -1,5 +1,12 @@
 use filuplex::ops::GpuBuffer;
+use std::path::Path;
 
 pub trait Layer {
     fn forward(&self) -> GpuBuffer;
+    // fn backward(&self, grad_output: &GpuBuffer) -> GpuBuffer;
+}
+
+pub trait Serializable {
+    fn save_to_file(&self, path: &Path) -> Result<(), Box<dyn std::error::Error>>;
+    fn load_from_file(&mut self, path: &Path) -> Result<(), Box<dyn std::error::Error>>;
 }
