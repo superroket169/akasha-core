4d28d9ce00aa34998bde06e90db06bf607a40fca
superroket169 <isagullu2010@gmail.com>
2026-06-23 13:29:20 +0300

    fix

--- src/nn/embedding.rs
+++ src/nn/embedding.rs
@@ -34,7 +34,7 @@ impl Embedding {
         let zero_table = vec![0.0 as Real; (vocab_size * dim) as usize];
         let grad_table = Arc::new(Tensor::init_from_cpu(ctx.clone(), &zero_table));
 
-        let meta_data = vec![vocab_size, dim];
+        let meta_data = vec![vocab_size, dim, seq_len];
         let t_meta = Arc::new(Tensor::init_from_cpu(ctx.clone(), &meta_data));
 
         let out_size = (seq_len * dim) as usize;
@@ -42,7 +42,6 @@ impl Embedding {
         let out_buffer = Arc::new(Tensor::init_from_cpu(ctx.clone(), &zero_out));
 
         let shader = BuiltInShader::Embedding.get_def();
-        let total_threads = seq_len * dim;
 
         // --- FORWARD ---
         let mut forward_graph = ComputeGraph::new(ctx.clone());
@@ -70,7 +69,7 @@ impl Embedding {
                     mode: TensorMode::Meta,
                 },
             ],
-            [(total_threads + 255) / 256, 1, 1],
+            [(dim + 255) / 256, seq_len, 1],
         );
 
         // --- BACKWARD ---
@@ -99,7 +98,7 @@ impl Embedding {
                     mode: TensorMode::Meta,
                 },
             ],
-            [(seq_len * dim + 255) / 256, 1, 1],
+            [(dim + 255) / 256, seq_len, 1],
         );
 
         Self {
