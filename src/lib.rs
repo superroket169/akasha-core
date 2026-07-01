33aca785bcdecc3380c4165f1ac6f637de7cf4ba
superroket169 <isagullu2010@gmail.com>
2026-07-01 10:00:29 +0300

    feat/update: updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+pub mod config;
+pub mod data;
 pub mod nn;
 pub mod optim;
 pub mod tokenizer;
 
-pub const READ_LOSS: usize = 1000;
+pub const READ_LOSS: usize = config::LOG_EVERY;
 pub type Real = f32;
