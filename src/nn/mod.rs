3b566a8654562ae652462e5c817040320f1851fc
superroket169 <isagullu2010@gmail.com>
2026-08-29 18:26:49 +0300

    refac: refactor preparetions, new doc design

--- src/nn/mod.rs
+++ src/nn/mod.rs
@@ -1,11 +1,13 @@
-pub mod block;
 pub mod checkpoint;
 pub mod inference;
 pub mod inference_graphs;
+pub mod inference_ops;
 pub mod layers;
 pub mod ops;
 pub mod sampling;
+pub mod tape;
 pub mod train;
+pub mod transformer_ops;
 pub mod weights;
 
 pub use inference::InferenceSession;
